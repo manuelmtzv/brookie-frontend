f10a2fd3ed9f0cfe9669c039fc0131030c4f8549
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-04-19 21:09:53 -0600

    Agregando variable de entorno de API URL

--- src/hooks/useFetchReviews.js
+++ src/hooks/useFetchReviews.js
@@ -1,39 +1,41 @@
-import { useUserContext } from "./useUserContext";
-import { useReviewContext } from "./useReviewsContext";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useUserContext } from './useUserContext'
+import { useReviewContext } from './useReviewsContext'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const API_URL = process.env.REACT_APP_API_URL
 
 export const useFetchReviews = () => {
-  const [error, setError] = useState(null); 
-  const [isLoading, setIsLoading] = useState(null); 
-  const { user  } = useUserContext();
-  const { dispatch } = useReviewContext(); 
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const { user } = useUserContext()
+  const { dispatch } = useReviewContext()
 
   useEffect(() => {
     if (user) {
       const fetchReviews = async () => {
-        setIsLoading(true); 
-        setError(null); 
-    
-        const response = await fetch("https://brookie-backend.onrender.com/review", {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch(`${API_URL}/review`, {
           headers: {
-            Authorization: `Bearer ${user.token}`
-          }
+            Authorization: `Bearer ${user.token}`,
+          },
         })
-        const json = await response.json()    
-    
+        const json = await response.json()
+
         if (!response.ok) {
-          setIsLoading(false); 
-          setError(JSON.parse(json.error)); 
+          setIsLoading(false)
+          setError(JSON.parse(json.error))
         }
-    
+
         if (response.ok) {
-          dispatch({ type: "SET_REVIEWS", payload: json })
-          setIsLoading(false); 
+          dispatch({ type: 'SET_REVIEWS', payload: json })
+          setIsLoading(false)
         }
       }
-      fetchReviews()  
-    }    
+      fetchReviews()
+    }
   }, [dispatch, user])
 
   return { isLoading, error }
